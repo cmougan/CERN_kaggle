477abcbe707b02f80cc0a055d71c9e4e587247de
david26694 <david26694@gmail.com>
2020-11-15 11:42:27 +0100

    More blending

--- models/blend.py
+++ models/blend.py
@@ -6,14 +6,16 @@ train_raw = pd.read_csv("data/train.csv").drop(columns="BUTTER")
 
 lgbm_df = pd.read_csv('data/blend/valid_lgbm.csv')
 fastai_df = pd.read_csv('data/blend/valid_fastai_nn_grt.csv')
-# fastai_df_q = pd.read_csv('data/blend/valid_fastai_nn.csv')
+fastai_df_q = pd.read_csv('data/blend/valid_fastai_nn.csv')
 fastai_df_single = pd.read_csv('data/blend/valid_fastai_nn_single_grt.csv')
-# fastai_df_single_q = pd.read_csv('data/blend/valid_fastai_nn_single.csv')
+fastai_df_single_q = pd.read_csv('data/blend/valid_fastai_nn_single.csv')
 resnet = pd.read_csv('data/blend/res1_valid_preds.csv')
 
 lgbm_submission = pd.read_csv("submissions/more_features_lgbm.csv")
 fastai_submission = pd.read_csv("submissions/fastai_nn_grt.csv")
+fastai_submission_q = pd.read_csv("submissions/fastai_nn.csv")
 fastai_submission_single = pd.read_csv("submissions/fastai_nn_grt_single.csv")
+fastai_submission_single_q = pd.read_csv("submissions/fastai_nn_single.csv")
 resnet_submission = pd.read_csv('submissions/resnet.csv')
 
 
@@ -31,18 +33,24 @@ optimal_w = 0
 
 for w_nn in np.linspace(0, 1, 50):
     print(f"weight fastai {w_nn:.2f}")
-    w_fastai = (2 * w_nn / 3)
-    w_resnet = (1 * w_nn / 3)
+    w_fastai = (4 * w_nn / 5)
+    w_resnet = (1 * w_nn / 5)
     w_lgbm = 1 - w_nn
-    fastai_contr = fastai_df["prediction"] ** (w_fastai / 2)
+    w_grt = (2 / 6) * w_fastai
+    w_no_grt = ((1 - 2 * w_grt) / 2) * w_fastai
+    fastai_contr = fastai_df["prediction"] ** (w_grt)
+    fastai_contr_q = fastai_df_q["prediction"] ** (w_no_grt)
     resnet_contr = resnet["Predicted"] ** (w_resnet)
-    fastai_contr_single = fastai_df_single["prediction"] ** (w_fastai / 2)
+    fastai_contr_single = fastai_df_single["prediction"] ** (w_grt)
+    fastai_contr_single_q = fastai_df_single_q["prediction"] ** (w_no_grt)
     lgmb_contr = lgbm_df["prediction"] ** (w_lgbm)
 
     roc = roc_auc_score(
         valid_target.signal,
         fastai_contr_single + \
+        fastai_contr_single_q + \
         fastai_contr + \
+        fastai_contr_q + \
         lgmb_contr + \
         resnet_contr
     )
@@ -59,13 +67,20 @@ blend_submission = lgbm_submission.copy()
 
 
 optimal_nn_w = optimal_w
+fastai_coef = (4 / 5) * (2 / 6) * optimal_nn_w
+fastai_coef_q = (4 / 5) * (1 / 6) * optimal_nn_w
+resnet_coef = (1 / 5)
+print(2 * fastai_coef + 2 * fastai_coef_q + resnet_coef + (1 - optimal_nn_w))
+
 blend_submission["Predicted"] = \
     (lgbm_submission["Predicted"] ** (1 - optimal_nn_w)) + \
-    (fastai_submission["Predicted"] ** (1 * optimal_nn_w / 3)) + \
-    (fastai_submission_single["Predicted"] ** (1 * optimal_nn_w / 3)) + \
-    (resnet_submission["Predicted"] ** (1 * optimal_nn_w / 3))
+    (fastai_submission["Predicted"] ** fastai_coef) + \
+    (fastai_submission_single["Predicted"] ** fastai_coef) + \
+    (resnet_submission["Predicted"] ** resnet_coef) + \
+    (fastai_submission_q["Predicted"] ** fastai_coef_q) + \
+    (fastai_submission_single_q["Predicted"] ** fastai_coef_q)
 
 blend_submission.to_csv(
-    "submissions/blend_lgbm_fastai_resnet_02_111_grt.csv",
+    "submissions/blend_fastai_resnet_415_2211.csv",
     index=False
 )
